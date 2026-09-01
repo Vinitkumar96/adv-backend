2965e23aacccaf404cec87f8f8120d28c2b85688
vinit <vinitkumar93341@gmail.com>
2026-09-01 19:52:28 +0530

    basic langchain

--- level4/index.js
+++ level4/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
+import { ChatGoogle } from "@langchain/google";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 dotenv.config();
 
 const app = express();
@@ -8,27 +10,44 @@ const PORT = 5000;
 
 app.use(express.json());
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
+app.get("/health", (req, res) => {
+  return res.json({ message: "server is healthy" });
 });
 
-app.post("/ai", async (req, res) => {
-  const { input } = req.body;
-  const response = await ai.interactions.create({
-    model: "gemini-2.5-flash",
-    input: input,
-    system_instruction:
-      "you are a 7 years old child and has a unamed mother and father as your creator",
-  });
+//without langchain
 
-  return res.status(200).json({
-    ai: response.output_text,
-  });
-});
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+// });
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello" });
-});
+// app.post("/ai", async (req, res) => {
+//   const { input } = req.body;
+//   const response = await ai.interactions.create({
+//     model: "gemini-2.5-flash",
+//     input: input,
+//     system_instruction:
+//       "you are a 7 years old child and has a unamed mother and father as your creator",
+//   });
+
+//   return res.status(200).json({
+//     ai: response.output_text,
+//   });
+// });
+
+
+// with langchain
+
+const llm = new ChatGoogleGenerativeAI({
+  model:"gemini-2.5-flash",
+})
+
+app.post("/ai", async (req,res)=>{
+  const {input} = req.body
+  const response = await llm.invoke(input)
+
+  return res.status(200).json({ "ai" : response.content})
+})
+
 
 app.listen(PORT, () => {
   console.log("servers started");
@@ -38,3 +57,4 @@ app.listen(PORT, () => {
 // 1) no relatime access to data ,  2) no memory  
 
 // ai agent = LLM  + REAL TIME DATA + MEMORY + ACTION  => CREATE THIS WIHT FRAMEWORK CALLED LANGCHAIN , LANGGRAPH
+// langchain is high level framework.. langgraph is used when workflow becomes complex...langsmith (debuggin tool like postman + logs for ai agents)
